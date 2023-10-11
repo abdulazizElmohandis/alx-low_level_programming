aeadcb61aedf3b74c2d1ba05edef07d2bd35501c
Abdulaziz <azizelmohandis@gmail.com>
2023-10-11 23:13:34 +0300

    :(

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include"main.h"
+
+/**
+ *
+ * Return: 1 if +, 0 if 0 and -1 if
+*/
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43 + 0);
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar(0 + 48);
+		return (0);
+	}
+	else
+	{
+		_putchar(45 + 0);
+		return (-1);
+	}
+}
---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,19 @@
+#include"main.h"
+
+/**
+ *
+ * Return: lastDigit
+*/
+
+int print_last_digit(int n)
+{
+	int lastDigit;
+
+	if (n < 0)
+		lastDigit = (n % 10) * -1;
+	else
+		lastDigit = n % 10;
+
+	_putchar('0' + lastdigit); 
+	return (lastDigit);
+}
